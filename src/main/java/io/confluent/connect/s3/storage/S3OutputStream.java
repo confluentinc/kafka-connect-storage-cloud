1a1dd9a06e6416961ff7c138d21875daa6f99262
Konstantine Karantasis <konstantine@confluent.io>
2016-12-31 20:54:10 -0800

    Add initial skeleton of Input/Output S3 streams based on Hadoop code.

---
+++ src/main/java/io/confluent/connect/s3/storage/S3OutputStream.java
@@ -0,0 +1,218 @@
+/*
+ * Copyright 2016 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.confluent.connect.s3.storage;
+
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.event.ProgressEvent;
+import com.amazonaws.event.ProgressListener;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AbortMultipartUploadRequest;
+import com.amazonaws.services.s3.model.CompleteMultipartUploadRequest;
+import com.amazonaws.services.s3.model.InitiateMultipartUploadRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PartETag;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.UploadPartRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+import io.confluent.connect.s3.util.StringUtils;
+
+/**
+ * (The implementation has borrowed the general structure from Hadoop's implementation
+ */
+public class S3OutputStream extends OutputStream {
+  private static final Logger log = LoggerFactory.getLogger(S3OutputStream.class);
+  private final AmazonS3Client s3;
+  private final String bucket;
+  private final String key;
+  private final String ssea;
+  private final ProgressListener progressListener;
+  private final int partSize;
+  private boolean closed;
+  private ByteBuffer buffer;
+  private MultipartUpload multiPartUpload;
+
+  public S3OutputStream(String bucket, String key, String ssea, long partSize, AmazonS3Client s3) {
+    this.s3 = s3;
+    this.bucket = bucket;
+    this.key = key;
+    this.ssea = ssea;
+    // TODO: Should enforce that in config as well?
+    if (partSize > Integer.MAX_VALUE) {
+      log.warn("Part size of multipart uploads reduced to 2^31 - 1 bytes (~2.14GB)");
+      this.partSize = Integer.MAX_VALUE;
+    } else {
+      this.partSize = (int) partSize;
+    }
+    this.closed = false;
+    this.buffer = ByteBuffer.allocate(this.partSize);
+    this.progressListener = new ConnectProgressListener();
+    this.multiPartUpload = null;
+    log.debug("Create S3OutputStream for bucket '{}' key '{}'", bucket, key);
+  }
+
+  @Override
+  public void write(int b) throws IOException {
+    buffer.put((byte) b);
+    if (!buffer.hasRemaining()) {
+      uploadPart();
+    }
+  }
+
+  @Override
+  public void write(byte[] b, int off, int len) throws IOException {
+    if (b == null) {
+      throw new NullPointerException();
+    } else if (off < 0 || off > b.length || len < 0 || (off + len) > b.length || (off + len) < 0) {
+      throw new IndexOutOfBoundsException();
+    } else if (len == 0) {
+      return;
+    }
+
+    if (buffer.remaining() < len) {
+      int firstPart = buffer.remaining();
+      buffer.put(b, off, firstPart);
+      uploadPart();
+      write(b, off + firstPart, len - firstPart);
+    } else {
+      buffer.put(b, off, len);
+    }
+  }
+
+  private void uploadPart() throws IOException {
+    if (multiPartUpload == null) {
+      multiPartUpload = newMultipartUpload();
+    }
+    multiPartUpload.uploadPart(new ByteArrayInputStream(buffer.array()), partSize);
+    buffer.reset();
+  }
+
+  @Override
+  public void close() throws IOException {
+    if (closed) {
+      return;
+    }
+    closed = true;
+    try {
+      if (multiPartUpload == null) {
+        putObject();
+      } else {
+        if (buffer.hasRemaining()) {
+          multiPartUpload.uploadPart(new ByteArrayInputStream(buffer.array()), buffer.position() + 1);
+        }
+        multiPartUpload.complete();
+      }
+      log.debug("Upload complete for bucket '{}' key '{}'", bucket, key);
+    } catch (Exception e) {
+      if (multiPartUpload != null) {
+        multiPartUpload.abort();
+      }
+    } finally {
+      buffer.clear();
+      super.close();
+    }
+  }
+
+  private ObjectMetadata newObjectMetadata() {
+    ObjectMetadata meta = new ObjectMetadata();
+    if (StringUtils.isNotBlank(ssea)) {
+      meta.setSSEAlgorithm(ssea);
+    }
+    return meta;
+  }
+
+  private MultipartUpload newMultipartUpload() throws IOException {
+    InitiateMultipartUploadRequest initRequest = new InitiateMultipartUploadRequest(bucket, key, newObjectMetadata());
+    try {
+      return new MultipartUpload(s3.initiateMultipartUpload(initRequest).getUploadId());
+    } catch (AmazonClientException e) {
+      // TODO: elaborate on the exception interpretation. If this is an AmazonServiceException,
+      // there's more info to be extracted.
+      throw new IOException("Unable to initiate MultipartUpload: " + e, e);
+    }
+  }
+
+  private void putObject() throws IOException {
+    log.debug("Executing regular upload for bucket '{}' key '{}'", bucket, key);
+    ObjectMetadata meta = newObjectMetadata();
+    meta.setContentLength(buffer.position() + 1);
+    PutObjectRequest putObjectRequest =
+        new PutObjectRequest(bucket, key, new ByteArrayInputStream(buffer.array()), meta)
+            .withGeneralProgressListener(progressListener);
+    try {
+      s3.putObject(putObjectRequest);
+    } catch (AmazonClientException e) {
+      throw new IOException("Regular upload failed", e.getCause());
+    }
+  }
+
+  private class MultipartUpload {
+    private final String uploadId;
+    private final List<PartETag> partETags;
+
+    public MultipartUpload(String uploadId) {
+      this.uploadId = uploadId;
+      this.partETags = new ArrayList<>();
+      log.debug("Initiated multi-part upload for bucket '{}' key '{}' with id '{}'", bucket, key, uploadId);
+    }
+
+    public void uploadPart(ByteArrayInputStream inputStream, int partSize) {
+      int currentPartNumber = partETags.size() + 1;
+      UploadPartRequest request = new UploadPartRequest()
+                                            .withBucketName(bucket)
+                                            .withKey(key)
+                                            .withUploadId(uploadId)
+                                            .withInputStream(inputStream)
+                                            .withPartNumber(currentPartNumber)
+                                            .withPartSize(partSize)
+                                            .withGeneralProgressListener(progressListener);
+      log.debug("Uploading part {} for id '{}'", currentPartNumber, uploadId);
+      partETags.add(s3.uploadPart(request).getPartETag());
+    }
+
+    public void complete() {
+      log.debug("Completing multi-part upload for key '{}', id '{}'", key, uploadId);
+      CompleteMultipartUploadRequest completeRequest =
+          new CompleteMultipartUploadRequest(bucket, key, uploadId, partETags);
+      s3.completeMultipartUpload(completeRequest);
+    }
+
+    public void abort() {
+      log.warn("Aborting multi-part upload with id '{}'", uploadId);
+      try {
+        s3.abortMultipartUpload(new AbortMultipartUploadRequest(bucket, key, uploadId));
+      } catch (Exception e) {
+        log.warn("Unable to abort multipart upload, you may need to purge uploaded parts: ", e);
+      }
+    }
+  }
+
+  // Dummy listener for now, just logs the event progress.
+  private static class ConnectProgressListener implements ProgressListener {
+    public void progressChanged(ProgressEvent progressEvent) {
+      log.debug("Progress event: " + progressEvent);
+    }
+  }
+}
