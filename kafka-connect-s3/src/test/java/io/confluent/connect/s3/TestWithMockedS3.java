9d8283e83ac186d2c43f4d318190603d61a5b2cd
Tin Mak <tin.mak@plutux.com>
2019-06-26 17:49:12 +0800

    Fix test verify message with SafeByteArrayFormat correctly

--- kafka-connect-s3/src/test/java/io/confluent/connect/s3/TestWithMockedS3.java
+++ kafka-connect-s3/src/test/java/io/confluent/connect/s3/TestWithMockedS3.java
@@ -31,10 +31,13 @@ import org.junit.Rule;
 import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.apache.commons.io.IOUtils;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -121,12 +124,32 @@ public class TestWithMockedS3 extends S3SinkConnectorTestBase {
       } else if (extension.startsWith(".bin")) {
         return readRecordsByteArray(bucketName, fileKey, s3, compressionType,
             S3SinkConnectorConfig.FORMAT_BYTEARRAY_LINE_SEPARATOR_DEFAULT.getBytes());
+      } else if (extension.startsWith(".sbin")) {
+          return readRecordsSafeByteArray(bucketName, fileKey, s3, compressionType);
       } else if (extension.startsWith(".customExtensionForTest")) {
         return readRecordsByteArray(bucketName, fileKey, s3, compressionType,
             "SEPARATOR".getBytes());
       } else {
         throw new IllegalArgumentException("Unknown extension: " + extension);
       }
+  }
+
+  public static Collection<Object> readRecordsSafeByteArray(String bucketName, String fileKey, AmazonS3 s3,
+                                                            CompressionType compressionType) throws IOException {
+    log.debug("Reading records from bucket '{}' key '{}': ", bucketName, fileKey);
+    InputStream in = s3.getObject(bucketName, fileKey).getObjectContent();
+
+    ByteBuffer buf = ByteBuffer.wrap(IOUtils.toByteArray(compressionType.wrapForInput(in)));
+    List<Object> records = new ArrayList<>();
+
+    while (buf.hasRemaining()) {
+        int recordLength = buf.getInt();
+        byte[] record = ByteBuffer.allocate(recordLength).array();
+        buf.get(record, 0 , recordLength);
+        records.add(record);
+    }
+
+    return records;
   }
 
   public static Collection<Object> readRecordsAvro(String bucketName, String fileKey, AmazonS3 s3) throws IOException {
