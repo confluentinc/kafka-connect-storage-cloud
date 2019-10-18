1efd76237f94dce1f5fc344969de5b18c22c3e0a
Cyrus Vafadari <cyrus@confluent.io>
2019-10-17 17:00:37 -0700

    Cleanup

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/avro/AvroRecordWriterProvider.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/avro/AvroRecordWriterProvider.java
@@ -87,7 +87,8 @@ public class AvroRecordWriterProvider implements RecordWriterProvider<S3SinkConn
               log.debug("Null valued record cannot be written to output as Avro. "
                   + "Skipping. Record Key: {}", record.key());
               return;
-            } else if (conf.nullValueBehavior().equalsIgnoreCase(BehaviorOnNullValues.FAIL.toString())) {
+            } else if (conf.nullValueBehavior().equalsIgnoreCase(
+                BehaviorOnNullValues.FAIL.toString())) {
               throw new ConnectException("Null valued records are not writeable with current "
                   + S3SinkConnectorConfig.BEHAVIOR_ON_NULL_VALUES_CONFIG + " 'settings.");
             } else {
