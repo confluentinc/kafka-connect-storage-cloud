55a40cd3aa7cdd6ba5a5ea28f95c542405db3781
Konstantine Karantasis <konstantine@confluent.io>
2019-10-23 07:31:01 -0700

    CC-6801: Retry effectively on retriable failures
    CC-6801: Add tests

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
@@ -22,6 +22,7 @@ import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.DataException;
+import org.apache.kafka.connect.errors.RetriableException;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 import org.apache.kafka.connect.sink.SinkTaskContext;
@@ -51,6 +52,7 @@ public class S3SinkTask extends SinkTask {
 
   private S3SinkConnectorConfig connectorConfig;
   private String url;
+  private long timeoutMs;
   private S3Storage storage;
   private final Set<TopicPartition> assignment;
   private final Map<TopicPartition, TopicPartitionWriter> topicPartitionWriters;
@@ -93,6 +95,7 @@ public class S3SinkTask extends SinkTask {
     try {
       connectorConfig = new S3SinkConnectorConfig(props);
       url = connectorConfig.getString(StorageCommonConfig.STORE_URL_CONFIG);
+      timeoutMs = connectorConfig.getLong(S3SinkConnectorConfig.RETRY_BACKOFF_CONFIG);
 
       @SuppressWarnings("unchecked")
       Class<? extends S3Storage> storageClass =
@@ -123,12 +126,11 @@ public class S3SinkTask extends SinkTask {
 
   @Override
   public void open(Collection<TopicPartition> partitions) {
-    // assignment should be empty, either because this is the initial call or because it follows a call to "close".
+    // assignment should be empty, either because this is the initial call or because it follows
+    // a call to "close".
     assignment.addAll(partitions);
     for (TopicPartition tp : assignment) {
-      TopicPartitionWriter writer =
-          new TopicPartitionWriter(tp, writerProvider, partitioner, connectorConfig, context, time);
-      topicPartitionWriters.put(tp, writer);
+      topicPartitionWriters.put(tp, newTopicPartitionWriter(tp));
     }
   }
 
@@ -177,7 +179,20 @@ public class S3SinkTask extends SinkTask {
     }
 
     for (TopicPartition tp : assignment) {
-      topicPartitionWriters.get(tp).write();
+      TopicPartitionWriter writer = topicPartitionWriters.get(tp);
+      try {
+        writer.write();
+      } catch (RetriableException e) {
+        log.error("Exception on topic partition {}: ", tp, e);
+        Long currentStartOffset = writer.currentStartOffset();
+        if (currentStartOffset != null) {
+          context.offset(tp, currentStartOffset);
+        }
+        context.timeout(timeoutMs);
+        writer = newTopicPartitionWriter(tp);
+        writer.failureTime(time.milliseconds());
+        topicPartitionWriters.put(tp, writer);
+      }
     }
   }
 
@@ -226,6 +241,17 @@ public class S3SinkTask extends SinkTask {
   // Visible for testing
   TopicPartitionWriter getTopicPartitionWriter(TopicPartition tp) {
     return topicPartitionWriters.get(tp);
+  }
+
+  private TopicPartitionWriter newTopicPartitionWriter(TopicPartition tp) {
+    return new TopicPartitionWriter(
+        tp,
+        writerProvider,
+        partitioner,
+        connectorConfig,
+        context,
+        time
+    );
   }
 
   // Visible for testing
