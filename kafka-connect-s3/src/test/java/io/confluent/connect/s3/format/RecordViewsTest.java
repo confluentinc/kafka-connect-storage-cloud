753f4eee6904042b215830cd18c289cb7bbfe1ba
Daniel Osvath <dosvath@confluent.io>
2020-04-29 17:09:27 -0700

    CC-7933: added more unit tests to RecordViewsTest

--- kafka-connect-s3/src/test/java/io/confluent/connect/s3/format/RecordViewsTest.java
+++ kafka-connect-s3/src/test/java/io/confluent/connect/s3/format/RecordViewsTest.java
@@ -1,7 +1,11 @@
 package io.confluent.connect.s3.format;
 
+import java.util.Date;
 import org.apache.kafka.common.record.TimestampType;
+import org.apache.kafka.connect.data.Schema;
+import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.data.Timestamp;
 import org.apache.kafka.connect.header.ConnectHeaders;
 import org.apache.kafka.connect.header.Headers;
 import org.apache.kafka.connect.sink.SinkRecord;
@@ -27,22 +31,86 @@ public class RecordViewsTest {
   }
 
   @Test
-  public void testHeaderConvertsToString() {
-    Headers headers = new ConnectHeaders()
-        .addString("string", "string")
-        .addInt("int", 12)
-        .addBoolean("boolean", false);
+  public void testGetExtension() {
+    assertEquals("", new ValueRecordView().getExtension());
+    assertEquals(".keys", new KeyRecordView().getExtension());
+    assertEquals(".headers", new HeaderRecordView().getExtension());
+  }
 
-    List<Struct> expected = Arrays.asList(
+  @Test
+  public void testRecordKeyValueViews() {
+    SinkRecord sampleRecord = getSampleSinkRecord();
+    assertEquals(sampleRecord.key(), new KeyRecordView().getView(sampleRecord));
+    assertEquals(sampleRecord.value(), new ValueRecordView().getView(sampleRecord));
+    assertEquals(sampleRecord.keySchema(), new KeyRecordView().getViewSchema(sampleRecord));
+    assertEquals(sampleRecord.valueSchema(), new ValueRecordView().getViewSchema(sampleRecord));
+  }
+
+  @Test
+  public void testHeaderConvertsToString() {
+    List<Struct> expectedHeaders = Arrays.asList(
         new Struct(SINGLE_HEADER_SCHEMA)
             .put("key", "string").put("value", "string"),
         new Struct(SINGLE_HEADER_SCHEMA).put("key", "int").put("value", "12"),
         new Struct(SINGLE_HEADER_SCHEMA).put("key", "boolean").put("value", "false"));
 
-    SinkRecord record = new SinkRecord(
-        "topic", 0, null, null, null, null, 0, 0L, TimestampType.NO_TIMESTAMP_TYPE, headers);
+    Object headerView = new HeaderRecordView().getView(getSampleSinkRecord());
+    assertEquals(expectedHeaders, headerView);
+  }
 
-    Object headerView = new HeaderRecordView().getView(record);
-    assertEquals(expected, headerView);
+  private SinkRecord getSampleSinkRecord() {
+    Headers headers = new ConnectHeaders()
+        .addString("string", "string")
+        .addInt("int", 12)
+        .addBoolean("boolean", false);
+
+    Schema keyschema = getExampleKeyStructSchema();
+    Schema valueschema = getExampleValueStructSchema();
+
+    return new SinkRecord(
+        "topic", 0,
+        keyschema, getExampleKeyStruct(keyschema),
+        valueschema, getExampleValueStruct(valueschema),
+        0, 0L, TimestampType.NO_TIMESTAMP_TYPE, headers);
+  }
+
+  private Schema getExampleKeyStructSchema() {
+    return SchemaBuilder.struct()
+        .field("recordKeyID", Schema.INT64_SCHEMA)
+        .field("keyString", Schema.STRING_SCHEMA)
+        .field("keyBytes", Schema.BYTES_SCHEMA)
+        .build();
+  }
+
+  private Struct getExampleKeyStruct(Schema structSchema) {
+    Date sampleDate = new Date(1111111);
+    sampleDate.setTime(0);
+    return new Struct(structSchema)
+        .put("recordKeyID", (long) 0)
+        .put("keyString", "theStringVal")
+        .put("keyBytes", "theBytes".getBytes());
+  }
+
+  private Schema getExampleValueStructSchema() {
+    return SchemaBuilder.struct()
+        .field("ID", Schema.INT64_SCHEMA)
+        .field("myString", Schema.STRING_SCHEMA)
+        .field("myBool", Schema.BOOLEAN_SCHEMA)
+        .field("myBytes", Schema.BYTES_SCHEMA)
+        .field("myDate", org.apache.kafka.connect.data.Date.SCHEMA)
+        .field("myTime", Timestamp.SCHEMA)
+        .build();
+  }
+
+  private Struct getExampleValueStruct(Schema structSchema) {
+    Date sampleDate = new Date(1111111);
+    sampleDate.setTime(0);
+    return new Struct(structSchema)
+        .put("ID", (long) 0)
+        .put("myString", "theStringVal")
+        .put("myBool", true)
+        .put("myBytes", "theBytes".getBytes())
+        .put("myDate", sampleDate)
+        .put("myTime", new Date(33333333));
   }
 }
