b43ed423b87747969e5a3aa014edd5c5a6d691ba
SanchayGupta1197 <sanchay.gupta@hashedin.com>
2020-10-28 12:41:59 +0530

    added s3 sink network IT

---
+++ kafka-connect-s3/src/test/java/io/confluent/connect/s3/integration/BaseConnectorNetworkIT.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright [2020 - 2020] Confluent Inc.
+ */
+
+package io.confluent.connect.s3.integration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.ListObjectsV2Request;
+import com.amazonaws.services.s3.model.ListObjectsV2Result;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import io.confluent.common.utils.IntegrationTest;
+import org.apache.kafka.connect.runtime.AbstractStatus;
+import org.apache.kafka.connect.runtime.rest.entities.ConnectorStateInfo;
+import org.apache.kafka.connect.util.clusters.EmbeddedConnectCluster;
+import org.apache.kafka.test.TestUtils;
+import org.junit.experimental.categories.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+@Category(IntegrationTest.class)
+public abstract class BaseConnectorNetworkIT {
+
+  private static final Logger log = LoggerFactory.getLogger(BaseConnectorNetworkIT.class);
+
+  protected static final long CONSUME_MAX_DURATION_MS = TimeUnit.SECONDS.toMillis(60);
+  protected static final long CONNECTOR_STARTUP_DURATION_MS = TimeUnit.SECONDS.toMillis(1200);
+  protected static final String JSON_FORMAT_CLASS = "io.confluent.connect.s3.format.json.JsonFormat";
+  protected static final String S3_BUCKET = "sink-test-bucket-101";
+
+  protected static AmazonS3 s3RootClient;
+
+  protected static PumbaPauseContainer pumbaPauseContainer;
+
+  protected EmbeddedConnectCluster connect;
+
+  protected void startConnect() throws IOException {
+    connect = new EmbeddedConnectCluster.Builder()
+        .name("s3-connect-cluster")
+        //.workerProps(props)
+        .build();
+    connect.start();
+  }
+
+  protected void stopConnect() {
+    connect.stop();
+  }
+
+  /**
+   * Wait up to {@link #CONNECTOR_STARTUP_DURATION_MS maximum time limit} for the connector with the
+   * given name to start the specified number of tasks.
+   *
+   * @param name the name of the connector
+   * @param numTasks the minimum number of tasks that are expected
+   * @return the time this method discovered the connector has started, in milliseconds past epoch
+   * @throws InterruptedException if this was interrupted
+   */
+  protected long waitForConnectorToStart(String name, int numTasks) throws InterruptedException {
+    TestUtils.waitForCondition(
+        () -> assertConnectorAndTasksRunning(name, numTasks).orElse(false),
+        CONNECTOR_STARTUP_DURATION_MS,
+        "Connector tasks did not start in time."
+    );
+    return System.currentTimeMillis();
+  }
+
+  /**
+   * Confirm that a connector with an exact number of tasks is running.
+   *
+   * @param connectorName the connector
+   * @param numTasks the minimum number of tasks
+   * @return true if the connector and tasks are in RUNNING state; false otherwise
+   */
+  protected Optional<Boolean> assertConnectorAndTasksRunning(String connectorName, int numTasks) {
+    try {
+      ConnectorStateInfo info = connect.connectorStatus(connectorName);
+      boolean result = info != null
+                       && info.tasks().size() >= numTasks
+                       && info.connector().state().equals(AbstractStatus.State.RUNNING.toString())
+                       && info.tasks().stream().allMatch(s -> s.state().equals(AbstractStatus.State.RUNNING.toString()));
+      return Optional.of(result);
+    } catch (Exception e) {
+      log.error("Could not check connector state info.", e);
+      return Optional.empty();
+    }
+  }
+
+  /**
+   * Creates root client that will be used to change bucket permissions.
+   * Prerequisite : Access key and Secret access key should be set as environment variables
+   */
+  protected void createS3RootClient() {
+    s3RootClient = AmazonS3ClientBuilder.standard()
+        .withCredentials(
+            new AWSStaticCredentialsProvider(
+                new BasicAWSCredentials(
+                    System.getenv("ROOT_USER_ACCESS_KEY_ID"),
+                    System.getenv("ROOT_USER_SECRET_ACCESS_KEY"))))
+        .withRegion("ap-south-1")
+        .build();
+  }
+
+
+  protected void waitForConnectorToCompleteSendingRecords(
+      long noOfRecordsProduced,
+      int flushSize,
+      String bucketname)
+      throws InterruptedException {
+    TestUtils.waitForCondition(
+      () -> assertSuccess(noOfRecordsProduced, flushSize, bucketname).orElse(false),
+      CONNECTOR_STARTUP_DURATION_MS,
+      "Connector could not send all records in time."
+    );
+  }
+
+  protected Optional<Boolean> assertSuccess(
+      long noOfRecordsProduced,
+      int flushSize,
+      String bucketname) {
+    try {
+      int noOfObjectsInS3 = getNoOfObjectsInS3(bucketname);
+      boolean result = noOfObjectsInS3 == Math.ceil(noOfRecordsProduced / flushSize);
+      return Optional.of(result);
+    } catch (Exception e) {
+      log.error("Could not check S3 state", e);
+      return Optional.empty();
+    }
+  }
+
+  protected int getNoOfObjectsInS3(String bucketName) {
+    ListObjectsV2Request req = new ListObjectsV2Request()
+        .withBucketName(bucketName)
+        .withMaxKeys(100);
+    ListObjectsV2Result result;
+    List<String> records = new ArrayList<>();
+
+    do {
+      result = s3RootClient.listObjectsV2(req);
+
+      for (S3ObjectSummary objectSummary : result.getObjectSummaries()) {
+        if (objectSummary.getSize() > 0) {
+          records.add(objectSummary.getKey());
+        }
+      }
+      // If there are more than maxKeys keys in the bucket, get a continuation token
+      // and list the next objects.
+      String token = result.getNextContinuationToken();
+      req.setContinuationToken(token);
+    } while (result.isTruncated());
+    return records.size();
+  }
+
+  protected long waitForFetchingStorageObjectsInS3(String bucketName, long expectedObjects)
+      throws InterruptedException {
+    return waitForFetchingStorageObjectsInS3(bucketName,
+        expectedObjects,
+        CONSUME_MAX_DURATION_MS);
+  }
+
+  protected long waitForFetchingStorageObjectsInS3(
+      String bucketName,
+      long expectedObjects,
+      long maxWaitMs)
+      throws InterruptedException {
+    long startTime = System.currentTimeMillis();
+    long fetchedObjects = -1;
+    while (System.currentTimeMillis() - startTime < maxWaitMs) {
+      // added intentional sleep to get a connection from the connection pool.
+      Thread.sleep(Math.min(maxWaitMs, 5000L));
+      fetchedObjects = getNoOfObjectsInS3(bucketName);
+      if (fetchedObjects == expectedObjects) {
+        break;
+      }
+    }
+    return fetchedObjects;
+  }
+
+  protected void startPumbaPauseContainer() {
+    pumbaPauseContainer = new PumbaPauseContainer();
+    pumbaPauseContainer.start();
+  }
+
+}
---
+++ kafka-connect-s3/src/test/java/io/confluent/connect/s3/integration/S3SinkConnectorNetworkIT.java
@@ -0,0 +1,334 @@
+/*
+ * Copyright [2020 - 2020] Confluent Inc.
+ */
+
+package io.confluent.connect.s3.integration;
+
+import com.amazonaws.auth.policy.Policy;
+import com.amazonaws.auth.policy.Principal;
+import com.amazonaws.auth.policy.Statement;
+import com.amazonaws.auth.policy.actions.S3Actions;
+import com.amazonaws.auth.policy.resources.S3ObjectResource;
+import com.amazonaws.services.s3.model.CreateBucketRequest;
+import com.amazonaws.services.s3.model.ListVersionsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import com.amazonaws.services.s3.model.S3VersionSummary;
+import com.amazonaws.services.s3.model.VersionListing;
+import io.confluent.common.utils.IntegrationTest;
+import io.confluent.connect.s3.S3SinkConnectorConfig;
+import io.confluent.testcontainers.squid.SquidProxy;
+import org.apache.kafka.connect.runtime.SinkConnectorConfig;
+import org.apache.kafka.connect.storage.StringConverter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.kafka.connect.runtime.ConnectorConfig.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+/**
+ * Integration test for S3 Sink Connector.<p>
+ * Refer file /bin/README.md to find prerequisites
+ * to run the following tests.
+ */
+@Category(IntegrationTest.class)
+public class S3SinkConnectorNetworkIT extends BaseConnectorNetworkIT {
+
+  private static final Logger log = LoggerFactory.getLogger(S3SinkConnectorIT.class);
+
+  private static final String CONNECTOR_NAME = "s3-sink-connector";
+  private static final long NUM_RECORDS_PRODUCED = 1000;
+  private static final int TASKS_MAX = 1;
+  private static final List<String> KAFKA_TOPICS = Arrays.asList("topic1");
+  private static final int FLUSH_SIZE = 200;
+  private static SquidProxy squid;
+
+  @Before
+  public void setup() throws IOException {
+    startConnect();
+    createS3RootClient();
+    // create the test bucket
+    createS3Bucket(S3_BUCKET);
+  }
+
+
+  @After
+  public void close() {
+    // delete the test bucket
+    deleteBucket(S3_BUCKET);
+    stopConnect();
+  }
+
+  /**
+   * Success scenario : End to end test
+   * @throws Throwable
+   */
+  @Test
+  //@Ignore
+  public void testToAssertConnectorAndDestinationRecords() throws Throwable {
+
+    // create topics in Kafka
+    KAFKA_TOPICS.forEach(topic -> connect.kafka().createTopic(topic, 1));
+    // send records to kafka
+    int totalNoOfRecordsProduced = sendRecordsToKafka();
+
+    Map<String, String> props = getConnectorProps();
+
+    // start a sink connector
+    connect.configureConnector(CONNECTOR_NAME, props);
+    // wait for tasks to spin up
+    int minimumNumTasks = Math.min(KAFKA_TOPICS.size(), TASKS_MAX);
+
+    waitForConnectorToStart(CONNECTOR_NAME, minimumNumTasks);
+    waitForConnectorToCompleteSendingRecords(totalNoOfRecordsProduced, FLUSH_SIZE, S3_BUCKET);
+
+    // assert records
+    assertEquals(totalNoOfRecordsProduced / FLUSH_SIZE, getNoOfObjectsInS3(S3_BUCKET));
+  }
+
+  /**
+   * Case in which bucket permissions/policies are changed while uploading records
+   * Prerequisite : Access key and Secret access key should be set as environment variables
+   * @throws Exception
+   */
+  @Test
+//  @Ignore
+  public void testWithRevokedWritePermissions() throws Exception {
+
+    addReadWritePolicyToBucket(S3_BUCKET);
+    KAFKA_TOPICS.forEach(topic -> connect.kafka().createTopic(topic, 1));
+
+    // send records to kafka
+    int totalNoOfRecordsProduced = sendRecordsToKafka();
+
+    Map<String, String> props = getConnectorProps();
+    props.put("aws.access.key.id", System.getenv("SECONDARY_USER_ACCESS_KEY_ID"));
+    props.put("aws.secret.access.key", System.getenv("SECONDARY_USER_SECRET_ACCESS_KEY"));
+
+    // start a sink connector
+    connect.configureConnector(CONNECTOR_NAME, props);
+    // wait for tasks to spin up
+    int minimumNumTasks = Math.min(KAFKA_TOPICS.size(), TASKS_MAX);
+
+    waitForConnectorToStart(CONNECTOR_NAME, minimumNumTasks);
+    waitForConnectorToCompleteSendingRecords(totalNoOfRecordsProduced, FLUSH_SIZE, S3_BUCKET);
+
+    // revoke read/write permission
+    alterReadWritePolicyOfBucket(S3_BUCKET);
+    /*
+     Intentional sleep added in order for the bucket permission to be altered to read only to
+     come into affect.
+    */
+    Thread.sleep(10000);
+    // produce more records to kafka
+    totalNoOfRecordsProduced += sendRecordsToKafka();
+    int idealObjectCountS3 = totalNoOfRecordsProduced / FLUSH_SIZE;
+    long actualObjectWrittenS3 = NUM_RECORDS_PRODUCED / FLUSH_SIZE;
+    assertNotEquals(idealObjectCountS3,
+        waitForFetchingStorageObjectsInS3(S3_BUCKET, idealObjectCountS3));
+    assertEquals(actualObjectWrittenS3,
+        waitForFetchingStorageObjectsInS3(S3_BUCKET, idealObjectCountS3));
+
+  }
+
+  @Test
+  //@Ignore
+  public void testWithNetworkUnavailability() throws Throwable {
+    // Setup Squid Proxy Container
+    setupSquidProxy();
+    // create topics in Kafka
+    KAFKA_TOPICS.forEach(topic -> connect.kafka().createTopic(topic, 1));
+    // send records to kafka
+    int totalNoOfRecordsProduced = sendRecordsToKafka();
+
+    Map<String, String> props = getConnectorProps();
+    props.put(S3SinkConnectorConfig.S3_PROXY_URL_CONFIG, "https://"
+        + squid.getContainerIpAddress() + ":" + squid.getMappedPort(3129));
+
+    // start a sink connector
+    connect.configureConnector(CONNECTOR_NAME, props);
+    // wait for tasks to spin up
+    int minimumNumTasks = Math.min(KAFKA_TOPICS.size(), TASKS_MAX);
+
+    waitForConnectorToStart(CONNECTOR_NAME, minimumNumTasks);
+
+    // assert records
+    int objectCountBeforeInterruption = totalNoOfRecordsProduced / FLUSH_SIZE;
+    assertEquals(objectCountBeforeInterruption,
+        waitForFetchingStorageObjectsInS3(S3_BUCKET, objectCountBeforeInterruption));
+
+    // Shutting down proxy to emulate network unavailability
+    shutdownSquidProxy();
+
+    totalNoOfRecordsProduced += sendRecordsToKafka();
+    int idealObjectCount = totalNoOfRecordsProduced / FLUSH_SIZE;
+
+    // asserting no additional records are added.
+    assertNotEquals(totalNoOfRecordsProduced / FLUSH_SIZE,
+        waitForFetchingStorageObjectsInS3(S3_BUCKET, idealObjectCount));
+    assertEquals(objectCountBeforeInterruption,
+        waitForFetchingStorageObjectsInS3(S3_BUCKET, idealObjectCount));
+  }
+
+  @Test
+  //@Ignore
+  public void testWithNetworkInterruption() throws Throwable {
+    /*
+     A small value is used to create enough request that the pumba container can cause network
+     interruptions
+    */
+    int flushSize = 3;
+
+    setupSquidProxy();
+    startPumbaPauseContainer();
+    // create topics in Kafka
+    KAFKA_TOPICS.forEach(topic -> connect.kafka().createTopic(topic, 1));
+    // send records to kafka
+    int totalNoOfRecordsProduced = sendRecordsToKafka();
+
+    Map<String, String> props = getConnectorProps();
+    props.put("flush.size", Integer.toString(flushSize));
+
+    // start a sink connector
+    connect.configureConnector(CONNECTOR_NAME, props);
+    // wait for tasks to spin up
+    int minimumNumTasks = Math.min(KAFKA_TOPICS.size(), TASKS_MAX);
+
+    waitForConnectorToStart(CONNECTOR_NAME, minimumNumTasks);
+    waitForConnectorToCompleteSendingRecords(totalNoOfRecordsProduced, flushSize, S3_BUCKET);
+    pumbaPauseContainer.close();
+    // assert records
+    assertEquals(totalNoOfRecordsProduced / flushSize, getNoOfObjectsInS3(S3_BUCKET));
+    shutdownSquidProxy();
+  }
+
+  private void addReadWritePolicyToBucket(String bucketName) {
+
+    Statement allowRestrictedWriteStatement = new Statement(Statement.Effect.Allow)
+      .withPrincipals(new Principal(System.getenv("SECONDARY_USER_ACCOUNT_ID")))
+      .withActions(S3Actions.GetObject, S3Actions.PutObject)
+      .withResources(new S3ObjectResource(bucketName, "*"));
+
+    Policy policy = new Policy().withStatements(allowRestrictedWriteStatement);
+
+    s3RootClient.setBucketPolicy(bucketName, policy.toJson());
+  }
+
+  private void alterReadWritePolicyOfBucket(String bucketName) {
+
+    Statement allowRestrictedWriteStatement = new Statement(Statement.Effect.Allow)
+      .withPrincipals(new Principal(System.getenv("SECONDARY_USER_ACCOUNT_ID")))
+      .withActions(S3Actions.GetObject)
+      .withResources(new S3ObjectResource(bucketName, "*"));
+
+    Policy policy = new Policy().withStatements(allowRestrictedWriteStatement);
+
+    s3RootClient.setBucketPolicy(bucketName, policy.toJson());
+  }
+
+  private int sendRecordsToKafka() {
+    // Send records to Kafka
+    int totalNoOfRecordsProduced = 0;
+    for (int i = 0; i < NUM_RECORDS_PRODUCED; i++) {
+      totalNoOfRecordsProduced++;
+      String kafkaTopic = KAFKA_TOPICS.get(i % KAFKA_TOPICS.size());
+      String kafkaKey = "simple-key-" + i;
+      String kafkaValue = "simple-message-" + i;
+      log.debug("Sending message {} with topic {} to Kafka broker {}", kafkaTopic, kafkaValue);
+      connect.kafka().produce(kafkaTopic, kafkaKey, kafkaValue);
+    }
+    return totalNoOfRecordsProduced;
+  }
+
+  private Map<String, String> getConnectorProps() {
+    Map<String, String> props = new HashMap<>();
+    props.put(SinkConnectorConfig.TOPICS_CONFIG, String.join(",", KAFKA_TOPICS));
+    props.put(CONNECTOR_CLASS_CONFIG, "io.confluent.connect.s3.S3SinkConnector");
+    props.put(TASKS_MAX_CONFIG, Integer.toString(TASKS_MAX));
+
+    props.put("s3.region", "ap-south-1");
+    props.put("s3.part.size", "5242880");
+    props.put("s3.bucket.name", S3_BUCKET);
+    props.put("flush.size", Integer.toString(FLUSH_SIZE));
+    props.put("storage.class", "io.confluent.connect.s3.storage.S3Storage");
+    props.put("partitioner.class", "io.confluent.connect.storage.partitioner.DefaultPartitioner");
+
+    // converters
+    props.put(KEY_CONVERTER_CLASS_CONFIG, StringConverter.class.getName());
+    props.put(VALUE_CONVERTER_CLASS_CONFIG, StringConverter.class.getName());
+
+    props.put("format.class", JSON_FORMAT_CLASS);
+    // license properties
+    return props;
+  }
+
+  private void deleteBucket(String bucketName) {
+    emptyBucket(bucketName);
+    // After all objects are deleted, delete the bucket.
+    s3RootClient.deleteBucket(bucketName);
+  }
+
+  private void emptyBucket(String bucketName) {
+    // delete all objects to empty bucket
+    ObjectListing objectListing = s3RootClient.listObjects(bucketName);
+    while (true) {
+      Iterator<S3ObjectSummary> objIter = objectListing.getObjectSummaries().iterator();
+      while (objIter.hasNext()) {
+        s3RootClient.deleteObject(bucketName, objIter.next().getKey());
+      }
+
+      if (objectListing.isTruncated()) {
+        objectListing = s3RootClient.listNextBatchOfObjects(objectListing);
+      } else {
+        break;
+      }
+    }
+    // delete versioned objects
+    VersionListing versionList = s3RootClient.listVersions(new ListVersionsRequest().withBucketName(bucketName));
+    while (true) {
+      Iterator<S3VersionSummary> versionIter = versionList.getVersionSummaries().iterator();
+      while (versionIter.hasNext()) {
+        S3VersionSummary vs = versionIter.next();
+        s3RootClient.deleteVersion(bucketName, vs.getKey(), vs.getVersionId());
+      }
+
+      if (versionList.isTruncated()) {
+        versionList = s3RootClient.listNextBatchOfVersions(versionList);
+      } else {
+        break;
+      }
+    }
+  }
+
+  private void createS3Bucket(String bucketName) {
+    if (!s3RootClient.doesBucketExistV2(bucketName)) {
+      s3RootClient.createBucket(new CreateBucketRequest(bucketName));
+    }
+  }
+
+  public static void setupSquidProxy() {
+    //squid = new SquidProxy("confluent-docker-internal.jfrog.io/confluentinc/connect-squid:1.0.0", "NONE");
+    squid = new SquidProxy("connect-squid-local:1.0.0", "NONE");
+    squid.start();
+  }
+
+  public static void shutdownSquidProxy() {
+    if (squid != null) {
+      squid.stop();
+    }
+  }
+
+}
