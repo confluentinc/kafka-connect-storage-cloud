e5ed20c33344e5667ddaff7a3584ffa5bd88fd81
Chris Olivier <chris.olivier@cerebras.net>
2021-12-13 16:57:05 -0800

    Cleanup

--- kafka-connect-s3/src/test/java/io/confluent/connect/s3/integration/BaseConnectorIT.java
+++ kafka-connect-s3/src/test/java/io/confluent/connect/s3/integration/BaseConnectorIT.java
@@ -20,7 +20,6 @@ import com.amazonaws.services.s3.model.ListObjectsV2Request;
 import com.amazonaws.services.s3.model.ListObjectsV2Result;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 import io.confluent.common.utils.IntegrationTest;
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashSet;
