7e07fe8f873af06b54ab1d9b27c50cd4cc45e244
Chris Olivier <chris.olivier@cerebras.net>
2021-12-21 08:22:08 -0800

    Remove unused imports

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
@@ -31,9 +31,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
