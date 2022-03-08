055e11b0f5ae2f8925110b370db22035659bec46
Chris Olivier <chris.olivier@cerebras.net>
2022-03-07 17:07:56 -0800

    Fix pint merge conflict

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/parquet/ParquetRecordWriterProvider.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/format/parquet/ParquetRecordWriterProvider.java
@@ -29,9 +29,11 @@ import io.confluent.connect.s3.storage.S3Storage;
 import io.confluent.connect.storage.format.RecordWriter;
 import io.confluent.connect.storage.format.RecordWriterProvider;
 import org.apache.avro.generic.GenericRecord;
+import org.apache.kafka.connect.data.Field;
 import org.apache.kafka.connect.data.Schema;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.parquet.avro.AvroParquetWriter;
+import org.apache.parquet.avro.AvroWriteSupport;
 import org.apache.parquet.hadoop.ParquetFileWriter;
 import org.apache.parquet.hadoop.ParquetWriter;
 import org.apache.parquet.io.OutputFile;
@@ -40,6 +42,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ParquetRecordWriterProvider extends RecordViewSetter
     implements RecordWriterProvider<S3SinkConnectorConfig> {
@@ -70,20 +74,31 @@ public class ParquetRecordWriterProvider extends RecordViewSetter
 
         @Override
         public void write(SinkRecord record) throws IOException {
-          if (schema == null) {
+          if (schema == null || writer == null) {
             schema = recordView.getViewSchema(record, true);
             log.info("Opening record writer for: {}", adjustedFilename);
             org.apache.avro.Schema avroSchema = avroData.fromConnectSchema(schema);
-
-            s3ParquetOutputFile = new S3ParquetOutputFile(storage, adjustedFilename);
-            writer = AvroParquetWriter
-                    .<GenericRecord>builder(s3ParquetOutputFile)
-                    .withSchema(avroSchema)
-                    .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
-                    .withDictionaryEncoding(true)
-                    .withCompressionCodec(storage.conf().parquetCompressionCodecName())
-                    .withPageSize(PAGE_SIZE)
-                    .build();
+            s3ParquetOutputFile = new S3ParquetOutputFile(storage, filename);
+            AvroParquetWriter.Builder<GenericRecord> builder =
+                AvroParquetWriter.<GenericRecord>builder(s3ParquetOutputFile)
+                .withSchema(avroSchema)
+                .withWriteMode(ParquetFileWriter.Mode.OVERWRITE)
+                .withDictionaryEncoding(true)
+                .withCompressionCodec(storage.conf().parquetCompressionCodecName())
+                .withPageSize(PAGE_SIZE);
+            if (schemaHasArrayOfOptionalItems(schema, /*seenSchemas=*/null)) {
+              // If the schema contains an array of optional items, then
+              // it is possible that the array may have null items during the
+              // writing process.  In this case, we set a flag so as not to
+              // incur a NullPointerException
+              log.debug(
+                  "Setting \"" + AvroWriteSupport.WRITE_OLD_LIST_STRUCTURE
+                  + "\" to false because the schema contains an array "
+                  + "with optional items"
+              );
+              builder.config(AvroWriteSupport.WRITE_OLD_LIST_STRUCTURE, "false");
+            }
+            writer = builder.build();
           }
           log.trace("Sink record with view {}: {}", recordView, record);
           Object value = avroData.fromConnectData(schema, recordView.getView(record, true));
@@ -106,6 +121,38 @@ public class ParquetRecordWriterProvider extends RecordViewSetter
         }
       }
     );
+  }
+
+  /**
+   * Check if any schema (or nested schema) is an array of optional items
+   * @param schema The shema to check
+   * @return 'true' if the schema contains an array with optional items.
+   */
+  /* VisibleForTesting */
+  public static boolean schemaHasArrayOfOptionalItems(Schema schema, Set<Schema> seenSchemas) {
+    // First, check for infinitely recursing schemas
+    if (seenSchemas == null) {
+      seenSchemas = new HashSet<>();
+    } else if (seenSchemas.contains(schema)) {
+      return false;
+    }
+    seenSchemas.add(schema);
+    switch (schema.type()) {
+      case STRUCT:
+        for (Field field : schema.fields()) {
+          if (schemaHasArrayOfOptionalItems(field.schema(), seenSchemas)) {
+            return true;
+          }
+        }
+        return false;
+      case MAP:
+        return schemaHasArrayOfOptionalItems(schema.valueSchema(), seenSchemas);
+      case ARRAY:
+        return schema.valueSchema().isOptional()
+          || schemaHasArrayOfOptionalItems(schema.valueSchema(), seenSchemas);
+      default:
+        return false;
+    }
   }
 
   private static class S3ParquetOutputFile implements OutputFile {
