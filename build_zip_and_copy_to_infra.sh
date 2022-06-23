0a57c6370e3db57edc19e8a1329531b087b456ff
inbrewj-udc <jason@urbandatacollective.com>
2022-06-23 15:42:03 +0100

    UDX-1216 tidies up build_zip_and_copy script

--- build_zip_and_copy_to_infra.sh
+++ build_zip_and_copy_to_infra.sh
@@ -2,9 +2,10 @@
 
 # e.g. ~/workshop/udx-infra/terraform/modules/data-lake
 PATH_TO_INFRA=$1
+OUTPUT_ZIP_NAME="confluentinc-kafka-connect-s3-10.0.8-UdxStreamPartitioner.zip"
 
 mvn clean install -DskipTests && \
 echo "Removing existing build in infra, just to be safe..." && \
-rm -f "$1/confluentinc-kafka-connect-s3-udc-edit-10.0.8.zip" || true && \
-echo "Copying ZIP archived build to $1" && \
-cp kafka-connect-s3/target/components/packages/confluentinc-kafka-connect-s3-10.0.8.zip "$1/confluentinc-kafka-connect-s3-udc-edit-10.0.8.zip"
+rm -f "$PATH_TO_INFRA/$OUTPUT_ZIP_NAME" || true && \
+echo "Copying ZIP archived build to $PATH_TO_INFRA" && \
+cp kafka-connect-s3/target/components/packages/confluentinc-kafka-connect-s3-10.0.8.zip "$PATH_TO_INFRA/$OUTPUT_ZIP_NAME"
