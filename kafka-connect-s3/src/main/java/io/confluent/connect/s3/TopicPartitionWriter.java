372853ff706c477971a733e91acb88fbd97fde7c
tonglin-newsbreak <tong.lin@newsbreak.com>
2022-11-01 14:24:25 +0800

    update schema check

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/TopicPartitionWriter.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/TopicPartitionWriter.java
@@ -309,7 +309,9 @@ public class TopicPartitionWriter {
       );
       currentSchemas.put(encodedPartition, valueSchema);
       nextState();
-      return true;
+      if (!currentValueSchema.name().equals(valueSchema.name())) {
+        return true;
+      }
     }
 
     SinkRecord projectedRecord = compatibility.project(record, null, currentValueSchema);
@@ -490,13 +492,16 @@ public class TopicPartitionWriter {
     return writer;
   }
 
+  // encodedPartition添加了schema id
   private String getCommitFilename(String encodedPartition) {
     String commitFile;
     if (commitFiles.containsKey(encodedPartition)) {
       commitFile = commitFiles.get(encodedPartition);
     } else {
       long startOffset = startOffsets.get(encodedPartition);
+      // avro的prefix去掉了schema_name
       String prefix = getDirectoryPrefix(encodedPartition);
+      //
       commitFile = fileKeyToCommit(prefix, startOffset);
       commitFiles.put(encodedPartition, commitFile);
     }
