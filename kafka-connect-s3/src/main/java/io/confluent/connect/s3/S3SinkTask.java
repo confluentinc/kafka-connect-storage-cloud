82dc3c391ca69820699dba39d7b031ca052f2287
Subhash Iyer <siyer@confluent.io>
2025-07-18 15:51:56 +0530

    Ignore expcetion in close task

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
@@ -331,7 +331,7 @@ public class S3SinkTask extends SinkTask {
     for (TopicPartition tp : topicPartitionWriters.keySet()) {
       try {
         topicPartitionWriters.get(tp).close();
-      } catch (ConnectException e) {
+      } catch (Exception e) {
         log.error("Error closing writer for {}. Error: {}", tp, e.getMessage());
       }
     }
